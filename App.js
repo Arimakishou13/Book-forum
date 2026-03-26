720fea83cb5855beddf9dcdda0136099978e7de6
Arimakishou13 <164094974+Arimakishou13@users.noreply.github.com>
2026-03-26 03:57:53 +0200

    Update Firebase configuration and enhance login logic

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "PUT YOUR API KEY",
-  authDomain: "PUT YOUR DOMAIN",
-  projectId: "PUT YOUR ID",
+  apiKey: "AIzaSyCSKcXAntIIrm-BEgfCi2dykBby2SMuY48",
+  authDomain: "book-club-13.firebaseapp.com",
+  projectId: "book-club-13",
+  storageBucket: "book-club-13.firebasestorage.app",
+  messagingSenderId: "321914571323",
+  appId: "1:321914571323:web:edfc6bc0ac802e6dd06a1e"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -14,6 +17,8 @@ const auth = getAuth(app);
 window.login = function() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
+
+  if(!email || !password) return;
 
   signInWithEmailAndPassword(auth, email, password)
     .then(() => alert("Logged in 🔥"))
